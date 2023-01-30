34819607bb65322afe000ac119f7541c9f81ffb9
ggomez301 <45319737+ggomez301@users.noreply.github.com>
2023-01-30 13:04:10 -0600

    Update concatenateEIF_v2.py

--- ConcatenateEIF_v2/concatenateEIF_v2.py
+++ ConcatenateEIF_v2/concatenateEIF_v2.py
@@ -4,7 +4,7 @@
 ##  SAM LLC
 ##  This python GUI script looks at EIF files and concatenates all files into one EIF file that can
 ##   be opened as a TXT file. Can be used for multiple file locations or if wanted put general path
-##   in and script can look in many different files to read EIF files.
+##   in and script can look in various existing files to read EIF files.
 ##   Instructions:
 ##   1. Copy directory of path needed to concatenate EIF files.
 ##   2. Paste the directory and hit submit.
